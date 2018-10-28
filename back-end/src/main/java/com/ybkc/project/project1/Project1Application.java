8d46fbf806bab8e631afc5fa77e72ca6f23dfe72
BokDaHoon <potter7050@linecorp.com>
2018-10-28 13:53:45 +0900

    Remove auto configuration for datasource

--- back-end/src/main/java/com/ybkc/project/project1/Project1Application.java
+++ back-end/src/main/java/com/ybkc/project/project1/Project1Application.java
@@ -1,9 +1,12 @@
 package com.ybkc.project.project1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Project1Application {
 
 	public static void main(String[] args) {

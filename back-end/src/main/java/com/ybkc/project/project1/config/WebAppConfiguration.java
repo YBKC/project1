685b503cfabecc5f4d5f11ffce53e1f47d8cbac8
BokDaHoon <potter7050@linecorp.com>
2018-11-04 12:26:20 +0900

    Add addResourceHandler for production mode

---
+++ back-end/src/main/java/com/ybkc/project/project1/config/WebAppConfiguration.java
@@ -0,0 +1,32 @@
+package com.ybkc.project.project1.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebAppConfiguration implements WebMvcConfigurer {
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        /* windows 용 경로 */
+        /*registry.addResourceHandler("/dist/**")
+                .addResourceLocations("file:////Users/user/Qusic_Server/qusic/src/main/resources/web/dist/");*/
+        /* linux 용 경로 */
+        registry.addResourceHandler("/dist/**")
+                .addResourceLocations("classpath:/web/dist/");
+        if (!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+        }
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/admin").setViewName("forward:/index.html");
+    }
+}
